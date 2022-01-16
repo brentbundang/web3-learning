7f4716fd2fdf3b2e2eea4a18b2eb96442b7f7a54
breent <brent.bundang@gmail.com>
2022-01-16 13:49:26 -0500

    Added brownie tests

--- demos/brownie_simple_storage/scripts/deploy.py
+++ demos/brownie_simple_storage/scripts/deploy.py
@@ -1,14 +1,16 @@
-from brownie import accounts, config
-
+from brownie import accounts, config, SimpleStorage
 
 #Provides the ability to get an account from the 10 auto generated
 #Sets the address, private key
 def deploy_simple_storage():
     account = accounts[0] 
-    #account = accounts.load("freecodecamp-account")
-    #print(account)
-    #account = accounts.add(config["wallets"]["from_key"])
-    print(account)
+    simple_storage = SimpleStorage.deploy({"from": account}) #Brownie is smart enough to know if it's a transaction or a call
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    transaction = simple_storage.store(15, {"from":account})
+    transaction.wait(1)
+    updated_store_value = simple_storage.retrieve({"from":account})
+    print(updated_store_value)
 
 def main():
     print("Hello!")
