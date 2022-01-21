30fc1e6102e1321738fec7feeb65c1b222cfdc6b
bbdang <bbdang@pop-os.localdomain>
2022-01-21 13:05:10 -0500

    Fixed an issue where the entrance fee was not applied correctly

--- demos/brownie_fund_me/scripts/deploy.py
+++ demos/brownie_fund_me/scripts/deploy.py
@@ -1,4 +1,3 @@
-from unittest.mock import Mock
 from brownie import FundMe, MockV3Aggregator, network,config 
 from scripts.helpful_scripts import get_account, deploy_mocks, LOCAL_BLOCKCHAIN_ENVIRONMENTS
 
@@ -16,6 +15,7 @@ def deploy_fund_me():
     #.get() will make our lives easier and we could run into index errors
     fund_me = FundMe.deploy(price_feed_address,{'from':account}, publish_source=config["networks"][network.show_active()].get("verify"))
     print(f"Contract deployed to {fund_me.address} ")
+    return fund_me
      
 def main():
     deploy_fund_me()
--- demos/brownie_fund_me/scripts/helpful_scripts.py
+++ demos/brownie_fund_me/scripts/helpful_scripts.py
@@ -2,7 +2,9 @@ from brownie import accounts, network, MockV3Aggregator, config
 from web3 import Web3
 
 DECIMALS = 8
-STARTING_PRICE = 200000000000
+# This is 2,000
+INITIAL_VALUE = 200000000000
+
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "local-ganache"]
 
 def get_account():
@@ -14,8 +16,7 @@ def get_account():
 def deploy_mocks():
     print(f"The active network is {network.show_active()}")
     print("Deploying mocks...")
-    if len(MockV3Aggregator) <=0:
-        MockV3Aggregator.deploy(DECIMALS,Web3.toWei(STARTING_PRICE,"ether"),{'from':get_account()})
-        print ("mocks deployed!")
+    MockV3Aggregator.deploy(DECIMALS,INITIAL_VALUE,{'from':get_account()})
+    print ("mocks deployed!")
              
  
