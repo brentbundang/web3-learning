8c5ed27b52995bc5214563b637691a1fdfcb5b2f
breent <brent.bundang@gmail.com>
2022-01-24 14:43:11 -0500

    Added the logic for VRF Coordinator

--- demos/smartcontract-lottery/contracts/Lottery.sol
+++ demos/smartcontract-lottery/contracts/Lottery.sol
@@ -1,10 +1,37 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >0.6.6
+pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
 
 contract Lottery {
-    function enter() public{}
-    function getEntranceFee() public {}
+    address payable[] public players;
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+
+    function enter() public payable{
+        //require();
+        //50USD minimum
+        players.push(msg.sender);    
+    }
+
+    constructor(address _priceFeed) public{
+         usdEntryFee = 50 * (10**18); //Expressed in wei
+         ethUsdPriceFeed = AggregatorV3Interface(_priceFeed);
+    }
+
+    function getEntranceFee() public view returns (uint256){
+        // Store the enterance fee
+        (,int256 answer,,,) = ethUsdPriceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(answer) * 10 ** 10; //18 decmial places due to a USD price feed
+        // 50, 2000 eth
+        // 50/2000
+        // 50 * 100000 / 2000
+        uint256 costToEnter = (usdEntryFee * 10 ** 18) / adjustedPrice;
+        return costToEnter;
+
+    }
     function startLottery() public {}
     function endLottery() public {}
 }
--- demos/smartcontract-lottery/readme.md
+++ demos/smartcontract-lottery/readme.md
@@ -1,3 +1,9 @@
 1. Users can enter the lottery with ETH based on a USD fee 
 2. An adnim will choose wehn the lottery is over
 3. The lottery will select a random winner 
+
+How do we want to test this? 
+
+1. Create a mainnet-fork
+2. Development with mocks
+3. Testnet
